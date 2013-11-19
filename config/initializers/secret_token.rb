8d7f0737669d8df9b024d50b01d2f78c9f3785d3
tuanlq <tuanlq@framgia.com>
2013-11-19 15:45:25 +0700

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rortweet::Application.config.secret_key_base = 'f756562c7e602003d7e633e26262d9354958a7424859c611950e5cd2c4706378e1b614d13d73ee5c9d658e4e66b4a3f7d7399dd271ec825534009c2f8efcb28f'
